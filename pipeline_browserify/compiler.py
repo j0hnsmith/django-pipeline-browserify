2a1b2a3bc8c3ddfed1fcbbe08b3de7cad06f6be2
Luke Hodkinson <furious.luke@gmail.coim>
2015-10-14 18:50:14 +1100

    Overloading `is_outdated` on the compiler class to properly catch all dependencies. I was finding that if I `required`d files that also lived in my static files folders from my entry-point files, then any changes to the dependent files would not result in recompiling the entry-point file.

--- pipeline_browserify/compiler.py
+++ pipeline_browserify/compiler.py
@@ -2,7 +2,10 @@ from __future__ import print_function
 
 from pipeline.compilers import SubProcessCompiler
 from os.path import dirname
+import json
 from django.conf import settings
+from django.contrib.staticfiles.finders import find as static_find
+from django.core.exceptions import SuspiciousFileOperation
 
 
 class BrowserifyCompiler(SubProcessCompiler):
@@ -27,3 +30,45 @@ class BrowserifyCompiler(SubProcessCompiler):
         print('\ncommand:', command)
         return self.execute_command(command, cwd=dirname(infile))
 
+    def is_outdated(self, infile, outfile):
+
+        # Check for missing file or modified entry-point file.
+        if super(BrowserifyCompiler, self).is_outdated(infile, outfile):
+            return True
+
+        # Check if we've already calculated dependencies.
+        deps = getattr(self, '_deps', None)
+        if not deps:
+
+            # Collect dependency information.
+            command = "%s %s %s --deps %s" % (
+                getattr(settings, 'PIPELINE_BROWSERIFY_VARS', ''),
+                getattr(settings, 'PIPELINE_BROWSERIFY_BINARY', '/usr/bin/env browserify'),
+                getattr(settings, 'PIPELINE_BROWSERIFY_ARGUMENTS', ''),
+                self.storage.path(infile),
+            )
+            dep_json = self.execute_command(command) #, cwd=dirname(infile))
+
+            # Process the output data. It's JSON, and the file's path is coded
+            # in the "file" field. We also want to save the content of each file
+            # so we can check if they're outdated, which is coded under "source".
+            deps = []
+            for dep in json.loads(dep_json.decode()):
+
+                # Is this file managed by the storage?
+                try:
+                    exists = self.storage.exists(dep['file'])
+                except SuspiciousFileOperation:
+                    exists = None
+                if exists == True or exists == False:
+                    deps.append(dep['file'])
+
+            # Cache the dependencies for the next possible run.
+            self._deps = deps
+
+        # Test the dependencies to see if they're out of date.
+        for dep in deps:
+            if super(BrowserifyCompiler, self).is_outdated(dep, outfile):
+                return True
+
+        return False
