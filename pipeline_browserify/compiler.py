545f0b140efb8e60bbe3c647e890fbbcc6d31b57
Moxlotus <moxlotus@live.com>
2016-02-16 21:20:26 +0800

    fixed the way attribute is retrieved

--- pipeline_browserify/compiler.py
+++ pipeline_browserify/compiler.py
@@ -2,7 +2,6 @@ from pipeline.compilers import SubProcessCompiler
 from os.path import dirname
 from django.conf import settings
 
-
 class BrowserifyCompiler(SubProcessCompiler):
     output_extension = 'browserified.js'
 
@@ -16,9 +15,9 @@ class BrowserifyCompiler(SubProcessCompiler):
             return
         pipeline_settings = getattr(settings, 'PIPELINE', {})
         command = "%s %s %s %s -o %s" % (
-            pipeline_settings['BROWSERIFY_VARS'] if 'BROWSERIFY_VARS' in pipeline_settings else '',
-            pipeline_settings['BROWSERIFY_BINARY'] if 'BROWSERIFY_BINARY' in pipeline_settings else '/usr/bin/env browserify',
-            pipeline_settings['BROWSERIFY_ARGUMENTS'] if 'BROWSERIFY_ARGUMENTS' in pipeline_settings else '',
+            pipeline_settings.get('BROWSERIFY_VARS', ''),
+            pipeline_settings.get('BROWSERIFY_BINARY', '/usr/bin/env browserify'),
+            pipeline_settings.get('BROWSERIFY_ARGUMENTS', ''),
             infile,   
             outfile,
         )
