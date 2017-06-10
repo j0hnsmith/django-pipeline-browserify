c150b39b53251e6531aa1719ea124faf25bba2db
David Koňařík <dvd.kon@gmail.com>
2017-06-10 11:36:04 +0200

    Add compatibility import for Python 2

--- pipeline_browserify/compiler.py
+++ pipeline_browserify/compiler.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 from pipeline.compilers import SubProcessCompiler
 from django.conf import settings
 from django.core.exceptions import SuspiciousFileOperation
